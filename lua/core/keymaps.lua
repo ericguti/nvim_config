17c76398c4bc17aabc8bea1e3672ab573b6795b7
guti <ericguri@hotmail.es>
2025-02-02 19:34:16 +0100

    Set up dir tree and init config

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+------------------------------------------
+------          PLUGINS        -----------
+------------------------------------------
+---Open neotree with \
+vim.keymap.set("n", "<Leader>\\", ":Neotree toggle<CR>", { noremap = true, silent = true })
+
+-- Keymap for Telescope pickers
+vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true }) -- Find files
+vim.keymap.set("n", "<Leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true }) -- Live grep
+vim.keymap.set("n", "<Leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })   -- Buffers
+vim.keymap.set("n", "<Leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true }) -- Help tags
---
+++ lua/core/options.lua
@@ -0,0 +1,11 @@
+vim.opt.autoindent = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.fileencoding = 'utf-8' 
+vim.opt.linebreak = true -- Do not break lines in the middle of the word
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+--vim.opt.tabstop = 4
+vim.opt.wrap = false
